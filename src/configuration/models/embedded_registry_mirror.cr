5c76932c236908ff3bd51b69e8a146141bce3830
sergeioboi <vitilus123@gmail.com>
2025-11-24 21:08:41 -0300

    - Move default private_registry_config to embedded_registry_mirror.cr - replace <<'EOF' with <<\EOF to fix writing configs having special bash symbols

--- src/configuration/models/embedded_registry_mirror.cr
+++ src/configuration/models/embedded_registry_mirror.cr
@@ -3,7 +3,10 @@ class Configuration::Models::EmbeddedRegistryMirror
   include YAML::Serializable::Unmapped
 
   getter enabled : Bool = true
-  getter private_registry_config : String? = nil
+  getter private_registry_config : String = <<-YAML
+  mirrors:
+    "*":
+  YAML
 
   def initialize
   end
